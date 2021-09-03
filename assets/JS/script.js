9ca8e206cf0d299d380b521eff6434d7493f1e67
Matthew Pandolfo <matt@pandolfo.com>
2021-09-02 21:17:23 -0400

    search cities for weather

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,10 +1,9 @@
 var apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
 
-function displayWeather() {
-  var toLoc = $("#toLoc").val().trim();
+function displayWeather(cityName) {
   var apiUrl =
     "http://api.openweathermap.org/data/2.5/forecast?q=" +
-    toLoc +
+    cityName +
     "&units=imperial" +
     "&appid=" +
     apiKey;
@@ -13,11 +12,22 @@ function displayWeather() {
       response.json().then(function (data) {
         console.log(data)
       })
+    } else {
+      M.toast({html: 'Please enter a valid city'});
     }
   });
 }
 
-$("#searchBtn").click(function (event) {
-  event.preventDefault();
-  displayWeather();
+function searchLoc() {
+  var toLoc = $("#toLoc").val().trim();
+  var cityName = toLoc.split(' ').join('%20');
+  if (cityName) {
+    displayWeather(cityName)
+  } else {
+    M.toast({html: 'Please enter a city'});
+  }
+}
+
+$("#searchBtn").click(function() {
+  searchLoc();
 });
