be47f66ad1d599be1f93a50f3a2f534d8424d12e
Matthew Pandolfo <matt@pandolfo.com>
2021-09-05 11:51:48 -0400

    fixed date

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,9 +1,9 @@
+const apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
 const weatherResults = document.querySelector('#weatherResults');
-var apiKey = "de81cbd7917a62a289c2cd964e2ccb23";
 
 function getWeather(cityName) {
   var apiUrl =
-    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
     "&units=imperial" +
     "&appid=" +
@@ -11,9 +11,22 @@ function getWeather(cityName) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
-        displayWeather(data);
-      })
+        
+        var apiUrlAll =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        data.coord.lat +
+        "&lon=" +
+        data.coord.lon +
+        "&units=imperial&appid=" +
+        apiKey;
+      fetch(apiUrlAll).then(function (response) {
+        response.json().then(function (data) {
+          // display data
+          console.log(data)
+          displayWeather(data);
+        });
+      });
+    });
     } else {
       M.toast({html: 'Please enter a valid city'});
     }
@@ -22,7 +35,7 @@ function getWeather(cityName) {
 
 function displayWeather(data) {
   for (let i = 0; i < 5; i++) {
-    var date = new Date(data.list[i].dt * 1000);
+    var date = new Date(data.daily[i].dt * 1000);
     var month = date.getMonth() +1;
     var day = date.getDate();
     var year = date.getFullYear();
@@ -30,7 +43,7 @@ function displayWeather(data) {
 
     var displayForecast = document.createElement('div');
     displayForecast.classList = 'col s2 weather-item';
-    displayForecast.textContent = fullDate;
+    displayForecast.textContent = month + '/' + day + '/' + year;
 
     // append el
     weatherResults.appendChild(displayForecast);
