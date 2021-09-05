379b42159749cacc6aa7990f8e0bd33a4142ef36
codemasterdev <skillz1029@gmail.com>
2021-09-05 18:17:59 -0400

    added jquery for calender pop up

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -14,6 +14,12 @@
 //     });
 // // API to convert location to Airport for 
 
+// for calender pop up 
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+});
+
+
 function skyscannerAPI(from, to, date) {
     let date1 = moment(date).format("YYYY-MM-DD");
     // let from = document.querySelector('#origin-input');
@@ -21,12 +27,12 @@ function skyscannerAPI(from, to, date) {
     let dateFormat = moment(date).format('MMM DD, YYYY');
 
     fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + from + "-sky/" + to + "-sky/" + date1, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-                "x-rapidapi-key": "1bde5c88fbmshc3edab4f94f2feep1ada8ejsn18b27f31e980"
-            }
-        })
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+            "x-rapidapi-key": "1bde5c88fbmshc3edab4f94f2feep1ada8ejsn18b27f31e980"
+        }
+    })
         .then(response => {
             console.log(response);
             if (response.Quotes.length === 0) {
@@ -61,17 +67,17 @@ function skyscannerAPI(from, to, date) {
         }).then(function () {
             $("#flight-table").trigger("update"); // sort table by flight departure date
         })
-    
 
 
-    .catch(err => {
-        console.error(err);
-    })
+
+        .catch(err => {
+            console.error(err);
+        })
 }
 
 $(document).ready(function () {
-  $("#start-date").datepicker({format: 'm-d-yyyy'});
-  $("#end-date").datepicker({format: 'm-d-yyyy'});
+    $("#start-date").datepicker({ format: 'm-d-yyyy' });
+    $("#end-date").datepicker({ format: 'm-d-yyyy' });
 
     $("#submit").on("click", function () {
         var origin = $("#origin-input").val().trim();
@@ -79,7 +85,7 @@ $(document).ready(function () {
         var startDate = $("#start-date").val().trim();
         var endDate = $("#end-date").val().trim();
         $(".flight").empty();
-        
+
         skyscannerAPI(cityToAirport[origin], cityToAirport[destination], startDate); //calling flight API for orgin to destination flight.
         skyscannerAPI(cityToAirport[destination], cityToAirport[origin], endDate); //calling flight API for return flight from destination to origin.
     });
