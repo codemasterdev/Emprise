aa7e74f7a7b5da1b78f91ce4733681b2dc864d68
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-09-07 13:32:53 -0400

    reformatted skyscanner API call to add async/await

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,51 +3,36 @@ $(document).ready(function () {
     $('.datepicker').datepicker();
 });
 
-
+//function to get user city input converted to API parameter format
 function skyscannerCity(cityCode) {
-
-    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" + cityCode, {
+    return fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" + cityCode, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
             "x-rapidapi-key": "1bde5c88fbmshc3edab4f94f2feep1ada8ejsn18b27f31e980"
         }
     })
-        .then(function (response) {
-            return response.json();
-        }).then(function (data) {
-            console.log(data);
-            console.log(data.Places[0].PlaceId);
-            return (data.Places[0].PlaceId);
-
-        })
-
-
-        .catch(err => {
-            console.error(err);
-        });
-
 }
 
-
-
 function skyscannerAPI(from, to, date) {
+    console.log(to);
+    console.log(from);
     let date1 = moment(date).format("YYYY-MM-DD");
     let dateFormat = moment(date).format('MMM DD, YYYY');
 
     fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + from + "/" + to + "/" + date1, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-            "x-rapidapi-key": "1bde5c88fbmshc3edab4f94f2feep1ada8ejsn18b27f31e980"
-        }
-    })
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+                "x-rapidapi-key": "1bde5c88fbmshc3edab4f94f2feep1ada8ejsn18b27f31e980"
+            }
+        })
         .then(function (response) {
             return response.json();
         }).then(function (data) {
-            // console.log(data.Body.Quotes);
+            console.log(data);
 
-            if (data.Body.Quotes.length === 0) {
+            if (data.Quotes.length === 0) {
                 var row2 = `
             <tr>
             <td>${"No Flights Available From " + from + " To " + to}</td>
@@ -59,21 +44,20 @@ function skyscannerAPI(from, to, date) {
             `
                 $(".flight").append(row2);
             } else {
-                for (i = 0; i < response.Carriers.length; i++) {
-                    if (response.Quotes[0].OutboundLeg.CarrierIds[0] == response.Carriers[i].CarrierId) {
+                for (i = 0; i < data.Carriers.length; i++) {
+                    if (data.Quotes[0].OutboundLeg.CarrierIds[0] == data.Carriers[i].CarrierId) {
                         var row2 = `
                 <tr>
                 <td>${from}</td>
                 <td>${to}</td>
-                <td>${response.Carriers[i].Name}</td>
+                <td>${data.Carriers[i].Name}</td>
                 <td>${dateFormat}</td>
-                <td>${"$" + response.Quotes[0].MinPrice}</td>
+                <td>${"$" + data.Quotes[0].MinPrice}</td>
                 </tr>
                 `
                         $(".flight").append(row2); //appends flight available to the table.
 
                     }
-
                 }
             }
         }).then(function () {
@@ -85,20 +69,27 @@ function skyscannerAPI(from, to, date) {
 
 $(document).ready(function () {
 
-    $("#submit").on("click", function () {
+    $("#submit").on("click", async function () {
         var destination = $("#destination-input").val().trim();
         var origin = $("#origin-input").val().trim();
         var startDate = $("#start-date").val().trim();
         var endDate = $("#end-date").val().trim();
         $(".flight").empty();
 
-        let start = skyscannerCity(origin);
-        console.log(start);
+        let startResponse = await skyscannerCity(origin);
+        console.log(startResponse);
+        let data = await startResponse.json();
 
-        let end = skyscannerCity(destination);
-        console.log(end);
+        console.log(data);
+let start = data.Places[0].PlaceId;
+console.log(start);
+        let endResponse = await skyscannerCity(destination);
+        console.log(endResponse);
+        let data2 = await endResponse.json();
+        let end = data2.Places[0].PlaceId;
+        console.log(data2);
 
-        skyscannerAPI(start, end, startDate); //calling flight API for orgin to destination flight.
+        await skyscannerAPI(start, end, startDate); //calling flight API for orgin to destination flight.
         skyscannerAPI(end, start, endDate); //calling flight API for return flight from destination to origin.
     });
 
